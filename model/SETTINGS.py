35400ac2fe9664f07745ad4d5dcd347024e48acd
Luki <hexeyxd@gmail.com>
2022-06-14 18:45:34 +0200

    gifcreator

--- model/SETTINGS.py
+++ model/SETTINGS.py
@@ -4,13 +4,14 @@ air_velocity = Velocity(2, 5, 2, 5, 0, 0)
 wall_velocity = Velocity(0, 0, 0, 0, 0, 0)
 pollution_rate = 0.2
 generalMap = []
-isDrawingBlocker = False
+#isDrawingBlocker = False
 window_size = (500, 500)
-n_layers = 6
-
+n_layers = 5
+gif_create=True
+gif_path= None
 
 #DRAWING
-multiple_layers_draw = False
+multiple_layers_draw = True
 
 #DEBUG
 TIMEPRINT = True
