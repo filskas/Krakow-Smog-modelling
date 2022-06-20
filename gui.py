3dc814596412d96a264e31d8ed0879c489137f89
Luki <hexeyxd@gmail.com>
2022-06-20 19:42:22 +0200

    a bit preetier gui code

--- gui.py
+++ gui.py
@@ -1,29 +1,19 @@
 import tkinter as tk
 import sys
 from PyQt5.QtWidgets import QApplication
+
 app = QApplication(sys.argv)
 screen = app.screens()[0]
-#dpi = screen.physicalDotsPerInch()
-dpi=100
-# from pyglet.window import key
-import random
-import numpy as np
-from PIL import Image as im
+# dpi = screen.physicalDotsPerInch()
+dpi = 100
+
 import matplotlib.pyplot as plt
 from PIL import Image
 from matplotlib.backend_bases import key_press_handler
-from matplotlib.backends._backend_tk import NavigationToolbar2Tk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-
 import gif_gen.gif_main
-import plotCSV
-import threading
 import mergemap
-from model.cube import Cube
 from model.map import *
-from model.velocity import Velocity
-from model.type import Type
-from model.layer import Layer
 from model.update import Update
 from utils import *
 from model.SETTINGS import *
@@ -37,8 +27,8 @@ def gui():
 
     x, z = 0, 0
 
-    height,width = window_size
-    frames = [[[WALL_COLOR for x in range(width+1)] for z in range(height+1)] for _ in range(n_layers)]
+    height, width = window_size
+    frames = [[[WALL_COLOR for x in range(width + 1)] for z in range(height + 1)] for _ in range(n_layers)]
     frames = np.uint8(frames)
     frames = np.array(frames)
 
@@ -92,11 +82,11 @@ def gui():
             print("trying to update")
             update()
         if key == 's':
-            for  cid in k_handlers:
+            for cid in k_handlers:
                 canvas.mpl_disconnect(cid)
 
             print("FOREVERLOOP, ONLY GOD CAN HELP US")
-            while(True):
+            while (True):
                 update()
                 redraw()
 
@@ -113,10 +103,11 @@ def gui():
 
     root = tk.Tk()
     root.wm_title("cracow smoker")
-    fig = plt.Figure(figsize=(float(width/(dpi*1.15)),float(height/(dpi*1.15))), dpi=dpi)
-    #fig.patch.set_facecolor('xkcd:mint green')
-    print(fig.get_figwidth(),fig.get_figheight())
-    _fr =   generalMap[cur_layer].getPixels(x, z, x + width, z + height)
+    fig = plt.Figure(figsize=(float(width / (dpi * 1.15)), float(height / (dpi * 1.15))),
+                     dpi=dpi)  # no idea why its not fullwindow image with just *1, but somehow 1.15 gave the best results
+    # fig.patch.set_facecolor('xkcd:mint green')
+    print(fig.get_figwidth(), fig.get_figheight())
+    _fr = generalMap[cur_layer].getPixels(x, z, x + width, z + height)
 
     if observing and draw_observers:
         drawObservers(_fr)
@@ -124,17 +115,22 @@ def gui():
     _fr = np.uint8(_fr)
 
     printIfDBG(("   framesstart", type(_fr), _fr.shape), TIMEPRINT)
-    image = fig.figimage( Image.fromarray(_fr, mode="RGBA"))
-
-    #generalMap[cur_layer].getPixelsToArray(x, z, x + width, z + height,frames,cur_layer,True)
-    #frames[cur_layer] = np.uint8(frames[cur_layer])
-    #image = fig.figimage( Image.fromarray(frames[cur_layer], mode="RGBA"))
+    image = fig.figimage(Image.fromarray(_fr, mode="RGBA"))
     canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
     canvas.draw()
 
     def redraw():
+        # clears the screen and
+        # if multiple layers flag is true ...
+        # draws (and saves if gif creation is enabled)
+        # all layers from the lowest to the one active
+        # all with alpha  .5
+
+        # otherwise
+        # draws only one active layer without any global alpha
+
         printIfDBG(("start", timeCheck()), TIMEPRINT)
-        nonlocal isDrawingBlocker
+        nonlocal isDrawingBlocker  # if new image is being drawn, block any incoming redraw commands
         nonlocal frames
         nonlocal image
         nonlocal x
@@ -156,6 +152,8 @@ def gui():
 
             for _i in range(cur_layer):
                 printIfDBG(("   gotPixels", timeCheck()), TIMEPRINT)
+
+                # observers are not implemented fully, so it has no impact
                 if observing and draw_observers:
                     drawObservers(frames[_i])
 
@@ -167,7 +165,7 @@ def gui():
                 if gif_create:
                     _pth = gif_path + "\\" + str(_i) + "\\" + str(iteration) + ".png"
                     image.save(_pth)
-                    printIfDBG(("saved img",_pth ),TIMEPRINT)
+                    printIfDBG(("saved img", _pth), TIMEPRINT)
                 fig.figimage(image, alpha=0.5)
                 print("drawed")
         else:
@@ -187,14 +185,11 @@ def gui():
         if gif_create:
             fig.savefig(gif_path + "\\full\\" + str(iteration) + ".png")
         printIfDBG(("   drawed", timeCheck()), TIMEPRINT)
-        printIfDBG( ("frames shape:", frames.shape ), TIMEPRINT)
+        printIfDBG(("frames shape:", frames.shape), TIMEPRINT)
         canvas.draw()
         isDrawingBlocker = False
         moved = False
 
-    # pack_toolbar=False will make it easier to use a layout manager later on.
-    #toolbar = NavigationToolbar2Tk(canvas, root, pack_toolbar=False)
-    #toolbar.update()
     k_handlers = []
     k_handlers.append(canvas.mpl_connect(
         "key_press_event", handle_keys
@@ -204,7 +199,7 @@ def gui():
 
     button_quit.pack(side=tk.BOTTOM)
     # slider_update.pack(side=tk.BOTTOM)
-    #toolbar.pack(side=tk.BOTTOM, fill=tk.X)
+    # toolbar.pack(side=tk.BOTTOM, fill=tk.X)
     canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
     tk.mainloop()
@@ -227,6 +222,7 @@ def load():
     generalMap = createMap(DATA, np.amin(DATA), np.amax(DATA), n_layers)
 
 
+# Main function
 def run():
     if observing:
         observeInit()
